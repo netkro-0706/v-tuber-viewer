d862c8bee8359a4e56a15ca98ec4eaca7a9502bf
netkro <kroging@gmail.com>
2025-04-21 23:05:19 +0900

    fix: container's width

--- src/app/components/layout/NavContainer.tsx
+++ src/app/components/layout/NavContainer.tsx
@@ -3,7 +3,7 @@ import { FC } from 'react'
 
 const NavContainder: FC<HasChildren> = ({ children }) => {
   return (
-    <div className="w-[1700px] mr-auto ml-auto h-[56px] flex items-center">
+    <div className="w-full max-w-[1700px] mr-auto ml-auto h-[56px] flex items-center">
       {children}
     </div>
   )
