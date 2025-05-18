4ad098b05542a1478d7f793bd4fe620d62e9977b
netkro <kroging@gmail.com>
2025-05-18 17:27:56 +0900

    feat: add metadata

--- src/app/(pages)/youtubePreview/page.tsx
+++ src/app/(pages)/youtubePreview/page.tsx
@@ -1,5 +1,16 @@
 import YoutubePreviewTemplate from '@/app/components/templates/YoutubePreviewTemplate'
+import { Metadata } from 'next'
 
-export default function YoutubeList() {
+export const metadata: Metadata = {
+  title: 'v-tuber-viewer',
+  description: 'v-tuber youtube previwer',
+  openGraph: {
+    title: 'v-tuber-viewer',
+    description: 'v-tuber youtube previwer',
+    images: ['/waktaverse.png'],
+  },
+}
+
+export default function YoutubeListPage() {
   return <YoutubePreviewTemplate />
 }
